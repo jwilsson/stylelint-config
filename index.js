7648d1e1c58c53b84f43874daa92a2f091a73fe7
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2017-02-24 13:19:59 +0100

    Allow 3 levels of nesting

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ module.exports = {
     "function-url-no-scheme-relative": true,
     "function-url-quotes": "always",
     "indentation": 4,
-    "max-nesting-depth": 2,
+    "max-nesting-depth": 3,
     "media-feature-name-no-vendor-prefix": true,
     "property-no-unknown": true,
     "property-no-vendor-prefix": true,
