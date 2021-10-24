95467be972a6040a925e410330812042abc9b437
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2021-10-24 11:34:58 +0200

    Use stylelint-config-standard-scss

--- packages/stylelint-config-scss/index.js
+++ packages/stylelint-config-scss/index.js
@@ -1,16 +1,7 @@
-const namePattern = /^[a-z]+([a-z0-9-]+[a-z0-9]+)?$/;
-
 module.exports = {
-    extends: '@jwilsson/stylelint-config',
+    extends: ['@jwilsson/stylelint-config', 'stylelint-config-standard-scss'],
     plugins: ['stylelint-scss'],
     rules: {
-        'at-rule-no-unknown': null,
-        'block-closing-brace-newline-after': [
-            'always',
-            {
-                ignoreAtRules: ['/^[a-z]*-mixin/', 'include'],
-            },
-        ],
         'selector-type-no-unknown': [
             true,
             {
@@ -18,35 +9,25 @@ module.exports = {
             },
         ],
 
-        'scss/at-else-if-parentheses-space-before': 'always',
-        'scss/at-extend-no-missing-placeholder': true,
         'scss/at-function-named-arguments': 'always',
         'scss/at-function-parentheses-space-before': 'always',
-        'scss/at-function-pattern': namePattern,
-        'scss/at-import-partial-extension': true,
         'scss/at-mixin-argumentless-call-parentheses': 'always',
         'scss/at-mixin-named-arguments': 'always',
         'scss/at-mixin-parentheses-space-before': 'always',
-        'scss/at-mixin-pattern': namePattern,
-        'scss/at-rule-no-unknown': true,
-        'scss/comment-no-empty': true,
-        'scss/dollar-variable-colon-space-after': 'always',
-        'scss/dollar-variable-colon-space-before': 'never',
-        'scss/dollar-variable-empty-line-after': true,
+        'scss/at-rule-conditional-no-parentheses': null,
+        'scss/dollar-variable-empty-line-after': [
+            'always',
+            {
+                except: 'before-dollar-variable',
+            },
+        ],
         'scss/dollar-variable-first-in-block': [
             true,
             {
                 ignore: ['comments'],
             },
         ],
-        'scss/dollar-variable-pattern': namePattern,
-        'scss/double-slash-comment-whitespace-inside': 'always',
-        'scss/function-unquote-no-unquoted-strings-inside': true,
         'scss/no-duplicate-dollar-variables': true,
-        'scss/no-duplicate-mixins': true,
-        'scss/no-global-function-names': true,
-        'scss/operator-no-unspaced': true,
-        'scss/percent-placeholder-pattern': namePattern,
         'scss/selector-no-redundant-nesting-selector': true,
         'scss/selector-no-union-class-name': true,
     },
